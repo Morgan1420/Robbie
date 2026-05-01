fd629aad6eb5ef6e55b5d6186ada8106432e281b
Morgan1420 <jan.moran.r@gmail.com>
2026-05-01 15:38:11 +0200

    motor actualitzat

--- motors.ino
+++ motors.ino
@@ -1,48 +1,37 @@
 // Motor A
 int en_dreta = 5;
-int en_esquerra = 6;
-int motor_dreta = 7;
-int motor_esquerra = 9;
+int in_dreta = 7;
 
 // Motor B
-int enB = 6;
-int in3 = 9;
-int in4 = 11;
+int en_esquerra = 9;
+int in_esquerra = 9;
 
 void setup() {
   // Configurem tots els pins com a sortida
-  pinMode(enA, OUTPUT);
-  pinMode(in1, OUTPUT);
-  pinMode(in2, OUTPUT);
-  pinMode(enB, OUTPUT);
-  pinMode(in3, OUTPUT);
-  pinMode(in4, OUTPUT);
+  pinMode(en_dreta, OUTPUT);
+  pinMode(in_dreta, OUTPUT);
+  pinMode(en_esquerra, OUTPUT);
+  pinMode(in_esquerra, OUTPUT);
 }
 
 void moureEndavant() {
   // Motor A endavant
-  digitalWrite(in1, HIGH);
-  digitalWrite(in2, LOW);
-  analogWrite(enA, 200); // Velocitat (0-255)
+  digitalWrite(in_dreta, HIGH);
+  analogWrite(en_dreta, 200); // Velocitat (0-255)
 
   // Motor B endavant
-  digitalWrite(in3, HIGH);
-  digitalWrite(in4, LOW);
-  analogWrite(enB, 200);
+  digitalWrite(in_esquerra, HIGH);
+  analogWrite(en_esquerra, 200);
 }
 
 void moureEnrere() {
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, HIGH);
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, HIGH);
+  digitalWrite(in_dreta, LOW);
+  digitalWrite(in_esquerra, LOW);
 }
 
 void parar() {
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, LOW);
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, LOW);
+  digitalWrite(in_dreta, LOW);
+  digitalWrite(in_esquerra, LOW);
 }
 
 void loop() {
